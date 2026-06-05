653ec412a2474d9fd1ff80f528aa35ea406af745
ミツキチ <mituki.tugui@gmail.com>
2026-06-05 11:11:22 +0900

    feat: Site・Page・Blockモデルを実装

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Block extends Model
 {
-    //
+    protected $fillable = [
+        'page_id',
+        'type',
+        'data',
+        'sort_order',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    public function page()
+    {
+        return $this->belongsTo(Page::class);
+    }
 }
