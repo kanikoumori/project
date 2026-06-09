9780c9e13aac84af80b8a4236a1328214f1dca37
ミツキチ <mituki.tugui@gmail.com>
2026-06-09 13:50:33 +0900

    profileの追加です

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/pages/{page}/histories', [PageHistoryController::class, 'index'])->name('pages.histories');
 
     Route::post('/histories/{history}/restore', [PageHistoryController::class, 'restore'])->name('histories.restore');
+
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
