47ad597e138313a0a55deb56bfcf6dd30b244d4e
onozaki <onozaki.rion1115@gmail.com>
2026-06-15 10:22:22 +0900

    editor画面cssの反映

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,10 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { Editor } from './editor/editor.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const editor = new Editor();
+    editor.initialize();
+});
