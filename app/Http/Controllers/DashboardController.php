f32081ededef7d5cb4608d88db2345ef75e8bc8d
kouryou <miyata.01.pc@gmail.com>
2026-06-28 17:39:58 +0900

    ホーム画面での進捗管理の変更

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,38 +8,75 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index()
-{
-    $sites = Site::where('user_id', auth()->id())
-        ->latest('updated_at')
-        ->get();
+    {
+        $sites = Site::where('user_id', auth()->id())
+            ->with(['pages' => function ($query) {
+                $query->withCount('blocks')
+                    ->orderBy('sort_order');
+            }])
+            ->latest('updated_at')
+            ->get();
 
-    $recentSites = $sites->take(3);
+        $recentSites = $sites->take(3);
 
-    $hasSite = $sites->isNotEmpty();
+        $totalSteps = 4;
 
-    $hasPage = $sites
-        ->load('pages')
-        ->pluck('pages')
-        ->flatten()
-        ->isNotEmpty();
+        $siteProgresses = $sites->mapWithKeys(function ($site) {
+            $hasSite = true;
 
-    $completedSteps = 0;
+            $hasPage = $site->pages->isNotEmpty();
 
-    if ($hasSite) {
-        $completedSteps++;
+            $hasEditedDesign = $site->pages->contains(function ($page) {
+                return $page->blocks_count > 0;
+            });
+
+            $completedSteps = 0;
+
+            if ($hasSite) {
+                $completedSteps++;
+            }
+
+            if ($hasPage) {
+                $completedSteps++;
+            }
+
+            if ($hasEditedDesign) {
+                $completedSteps++;
+            }
+
+            return [
+                $site->id => [
+                    'completedSteps' => $completedSteps,
+                    'hasSite' => $hasSite,
+                    'hasPage' => $hasPage,
+                    'hasEditedDesign' => $hasEditedDesign,
+                ],
+            ];
+        })->toArray();
+
+        $completedSteps = $sites->isNotEmpty()
+            ? $siteProgresses[$sites->first()->id]['completedSteps']
+            : 0;
+
+        $sitePages = $sites->mapWithKeys(function ($site) {
+            return [
+                $site->id => $site->pages->map(function ($page) {
+                    return [
+                        'id' => $page->id,
+                        'title' => $page->title,
+                        'url' => route('editor.show', $page),
+                    ];
+                })->values()->all(),
+            ];
+        })->toArray();
+
+        return view('dashboard.index', compact(
+            'sites',
+            'recentSites',
+            'completedSteps',
+            'totalSteps',
+            'sitePages',
+            'siteProgresses'
+        ));
     }
-
-    if ($hasPage) {
-        $completedSteps++;
-    }
-
-    $totalSteps = 4;
-
-    return view('dashboard.index', compact(
-        'sites',
-        'recentSites',
-        'completedSteps',
-        'totalSteps'
-    ));
-}
 }
